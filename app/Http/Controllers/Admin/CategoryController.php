ce6b5b93857990cccb34d73c7ff8c36a757caa18
liuxinghao <18395614665@163.com>
2019-01-27 11:37:23 +0800

    文章分类添加

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,16 +12,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Model\Category;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends CommonController
 {
-    //get  admin/category
+    //get  admin/category   分类列表
     public function index()
     {
         $data = (new Category)->tree();
         return view('admin.category.index')->with('data', $data);
     }
-
+    /*
+     * 修改分类排序
+     */
     public function changeOrder()
     {
         $input = Input::all();
@@ -47,14 +50,48 @@ class CategoryController extends CommonController
             return $return_data;
         }
     }
+    //get  admin/category/create  添加分类
+    public function create()
+    {
+        $where_sql['cate_pid'] = 0;
+        $data = (new Category)->where($where_sql)->orderBy('cate_id','desc')->get();
+        return view('admin/category/add',compact('data'));
+    }
     //post  admin/category
     public function store()
     {
-    }
-    //get  admin/category/create
-    public function create()
-    {
+        if ($input = Input::except('_token'))
+        {
+            $rules = [
+                'cate_name'=>'required',
+            ];
 
+            $message = [
+                'cate_name.required' => '分类名称不能为空！',
+            ];
+            $validator = Validator::make($input, $rules, $message);
+
+            if ($validator->passes())
+            {
+                $str = Category::create($input);
+                if ($str)
+                {
+                    return redirect('admin/category');
+                }
+                else
+                {
+                    return back()->with('errors','创建错误！');
+                }
+            }
+            else
+            {
+                return back()->withErrors($validator);
+            }
+        }
+        else
+        {
+            return view('admin/category/create');
+        }
     }
     //get  admin/category/{category}
     public function show()
--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -4,7 +4,7 @@
     <!--面包屑导航 开始-->
     <div class="crumb_warp">
         <!--<i class="fa fa-bell"></i> 欢迎使用登陆网站后台，建站的首选工具。-->
-        <i class="fa fa-home"></i> <a href="#">首页</a> &raquo; <a href="#">商品管理</a> &raquo; 添加商品
+        <i class="fa fa-home"></i> <a href="{{url('admin/info')}}">首页</a> &raquo; 添加商品
     </div>
     <!--面包屑导航 结束-->
 
@@ -12,6 +12,17 @@
     <div class="result_wrap">
         <div class="result_title">
             <h3>快捷操作</h3>
+            @if(count($errors) > 0)
+                <div class="mark">
+                    @if(is_object($errors))
+                        @foreach($errors->all() as $error)
+                            <p>{{$error}}</p>
+                        @endforeach
+                    @else
+                        {{$errors}}
+                    @endif
+                </div>
+            @endif
         </div>
         <div class="result_content">
             <div class="short_wrap">
@@ -24,69 +35,49 @@
     <!--结果集标题与导航组件 结束-->
 
     <div class="result_wrap">
-        <form action="#" method="post">
+        <form action="{{url('admin/category')}}" method="post">
+            {{csrf_field()}}
             <table class="add_tab">
                 <tbody>
                 <tr>
-                    <th width="120"><i class="require">*</i>分类：</th>
+                    <th width="120"><i class="require">*</i>父级分类：</th>
                     <td>
-                        <select name="">
+                        <select name="cate_pid">
                             <option value="">==请选择==</option>
-                            <option value="19">精品界面</option>
-                            <option value="20">推荐界面</option>
+                            @foreach($data as $v)
+                                <option value="{{$v['cate_id']}}">{{$v['cate_name']}}</option>
+                            @endforeach
                         </select>
                     </td>
                 </tr>
                 <tr>
-                    <th><i class="require">*</i>标题：</th>
+                    <th><i class="require">*</i>分类名称：</th>
                     <td>
-                        <input type="text" class="lg" name="">
-                        <p>标题可以写30个字</p>
+                        <input type="text" name="cate_name">
                     </td>
                 </tr>
                 <tr>
-                    <th>作者：</th>
+                    <th><i class="require">*</i>分类标题：</th>
                     <td>
-                        <input type="text" name="">
-                        <span><i class="fa fa-exclamation-circle yellow"></i>这里是默认长度</span>
+                        <input type="text" class="lg" name="cate_title">
                     </td>
                 </tr>
                 <tr>
-                    <th><i class="require">*</i>价格：</th>
+                    <th>分类关键词：</th>
                     <td>
-                        <input type="text" class="sm" name="">元
-                        <span><i class="fa fa-exclamation-circle yellow"></i>这里是短文本长度</span>
+                        <textarea name="cate_keywords"></textarea>
                     </td>
                 </tr>
                 <tr>
-                    <th><i class="require">*</i>缩略图：</th>
-                    <td><input type="file" name=""></td>
-                </tr>
-                <tr>
-                    <th>单选框：</th>
+                    <th>分类描述：</th>
                     <td>
-                        <label for=""><input type="radio" name="">单选按钮一</label>
-                        <label for=""><input type="radio" name="">单选按钮二</label>
+                        <textarea name="cate_description"></textarea>
                     </td>
                 </tr>
                 <tr>
-                    <th>复选框：</th>
+                    <th><i class="require">*</i>分类排序：</th>
                     <td>
-                        <label for=""><input type="checkbox" name="">复选框一</label>
-                        <label for=""><input type="checkbox" name="">复选框二</label>
-                    </td>
-                </tr>
-                <tr>
-                    <th>描述：</th>
-                    <td>
-                        <textarea name="discription"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <th>详细内容：</th>
-                    <td>
-                        <textarea class="lg" name="content"></textarea>
-                        <p>标题可以写30个字</p>
+                        <input type="text" name="cate_order">
                     </td>
                 </tr>
                 <tr>
